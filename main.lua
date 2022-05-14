ace20f674ab662eb7769b2e05ea49f8452fdb81c
oysi <oysi93@gmail.com>
2022-05-14 06:57:12 +0200

    changes

--- main.lua
+++ main.lua
@@ -1,114 +1,88 @@
 
-
 if not love then
-	print("lovec " .. arg[0]:match("(.+)[/\\]"))
 	os.execute("lovec " .. arg[0]:match("(.+)[/\\]"))
 	return
 end
 
---[==============================================[
-	code
-]==============================================]--
-
-local force_new = false
-
-function love.load(a, b)
-	-- print("a")
-	-- for i, v in pairs(a) do
-	-- 	print(i, v)
-	-- end
-	-- print("b")
-	-- for i, v in pairs(b) do
-	-- 	print(i, v)
-	-- end
-end
-
-require "./lib/Info"
-require "./lib/Map"
-require "./lib/Grid"
-require "./lib/Bar"
+local force = false
 
 print("Creating directories...")
 
-love.filesystem.createDirectory("sources")
-love.filesystem.createDirectory("textures")
+love.filesystem.createDirectory("src_cache")
+love.filesystem.createDirectory("lib_cache")
+
+love.filesystem.createDirectory("png")
 love.filesystem.createDirectory("tga")
 
 print("Checking lib...")
 
 for _, file in ipairs(love.filesystem.getDirectoryItems("lib")) do
 	local code = love.filesystem.read("lib/" .. file)
-	local read = love.filesystem.read("sources/" .. file .. ".txt")
-	if code ~= read then
+	local cache = love.filesystem.read("lib_cache/" .. file)
+	
+	local func, err = loadstring(code)
+	if not func then
+		error(err)
+	end
+	func()
+	
+	if code ~= cache then
 		print("Libraries have been changed, re-creating all textures...")
-		love.filesystem.write("sources/" .. file .. ".txt", code)
-		force_new = true
+		love.filesystem.write("lib_cache/" .. file, code)
+		force = true
 	end
 end
 
-print("Checking...")
-print("")
-
-local files = love.filesystem.getDirectoryItems("src")
+print("Checking src...")
 
 local verified = {}
 
-for k, file in ipairs(files) do
-	local func, err = love.filesystem.load("src/" .. file)
-	if not func then
-		error(err, 2)
-	end
-	
-	local img_name = file:sub(1, -5) .. ".png"
-	print("img_name", img_name)
-	
+for _, file in ipairs(love.filesystem.getDirectoryItems("src")) do
+	local name = file:sub(1, -5)
 	local code = love.filesystem.read("src/" .. file)
-	local read = love.filesystem.read("sources/" .. file .. ".txt")
+	local cache = love.filesystem.read("src_cache/" .. file)
 	
-	local texture_file = love.filesystem.getInfo("textures/" .. img_name)
-	
-	print("texture_file", texture_file)
-	
-	if not texture_file or code ~= read or force_new then
-		print(file .. " updating...")
+	if code ~= cache
+	or force
+	or not love.filesystem.getInfo("png/" .. name .. ".png")
+	or not love.filesystem.getInfo("tga/" .. name .. ".tga")
+	then
+		local func, err = loadstring(code)
+		if not func then
+			error(err)
+		end
 		
 		local t = os.clock()
 		
-		-- love.filesystem.remove("textures/" .. file:match("%d+") .. "*.png")
+		local imagedata = func()
 		
-		local imagedata = func(file)
+		imagedata:encode("png", "png/" .. name .. ".png")
+		imagedata:encode("tga", "tga/" .. name .. ".tga")
 		
-		-- imagedata:encode("png", file .. "_" .. imagedata:getWidth() .. "x" .. imagedata:getHeight() .. ".png")
-		imagedata:encode("png", "textures/" .. img_name)
-		imagedata:encode("tga", "tga/" .. file:sub(1, -5) .. ".tga")
-		
-		love.filesystem.write("sources/" .. file .. ".txt", code)
+		love.filesystem.write("src_cache/" .. file, code)
 		
 		t = os.clock() - t
 		
-		print(file .. " done! (" .. t .. "s)")
+		print(name .. " done! (" .. string.format("%.0f ms", t*1000) .. ")")
 	end
 	
-	verified[img_name] = true
+	verified[name] = true
 end
 
-print("")
-print("Checking done!")
-
-print("Removing old textures...")
-
-for k, file in ipairs(love.filesystem.getDirectoryItems("textures")) do
-	if not verified[file] then
-		print("Removing " .. "textures/" .. file .. "...")
-		love.filesystem.remove("textures/" .. file)
+for _, file in ipairs(love.filesystem.getDirectoryItems("png")) do
+	if not verified[file:sub(1, -5)] then
+		love.filesystem.remove("png/" .. file)
 	end
 end
 
-function love.update()
+for _, file in ipairs(love.filesystem.getDirectoryItems("tga")) do
+	if not verified[file:sub(1, -5)] then
+		love.filesystem.remove("tga/" .. file)
+	end
 end
 
 os.execute('powershell "' .. arg[1] .. "\\open-explorer.ps1" .. '"')
 
-print("done!")
+print("Done!")
 
 love.event.quit()
--- open-explorer.ps1
+++ open-explorer.ps1
@@ -4,5 +4,5 @@ $windows = (new-object -comobject shell.application).windows()
 $window = $windows | where { $_.locationurl | findstr ("$env:appdata\LOVE\love-texture-creator" -replace "\\","/") }
 
 if (-not($window)) {
-	explorer "$env:appdata\LOVE\love-texture-creator\textures"
+	explorer "$env:appdata\LOVE\love-texture-creator\png"
 }

6a7b6a2c170852fee17c8eccecbf75de94982f21
oysi-desktop <oysi93@gmail.com>
2022-09-19 17:31:44 +0200

    wallpaper flat color

--- src/bar.lua
+++ src/bar.lua
@@ -8,18 +8,10 @@ local imagedata = love.image.newImageData(256, 256)
 
 local info = Info(imagedata)
 
-local pad = 16
+local pad = 8
 
 Map(info, function()
-	local c
-	if info.y < pad then
-		c = 0.9
-	elseif info.y >= info.h - pad then
-		c = 0.5
-	else
-		local t = (info.y - pad)/(info.h - pad*2 - 1)
-		c = 0.8 - 0.2*t
-	end
+	local c = 0.6 + 0.1*(1 - math.abs(info.yd)^2)
 	return c, c, c, 1
 end)
 
